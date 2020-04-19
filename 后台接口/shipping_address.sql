11889bdf56edeae6e001619462616706f887556e
XDDDB <51122108+XDDDB@users.noreply.github.com>
2020-04-19 19:48:09 +0800

    Add files via upload

---
+++ 后台接口/shipping_address.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 8.0.16 : Database - xiangmu
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`xiangmu` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `xiangmu`;
+
+/*Table structure for table `shipping_address` */
+
+DROP TABLE IF EXISTS `shipping_address`;
+
+CREATE TABLE `shipping_address` (
+  `consignee` varchar(20) NOT NULL,
+  `account` char(20) NOT NULL,
+  `region` varchar(20) NOT NULL,
+  `detailed_address` varchar(100) NOT NULL,
+  `phone-number` varchar(11) NOT NULL,
+  `tel` int(20) DEFAULT NULL,
+  `postcode` int(10) NOT NULL,
+  `acquiescent` int(1) DEFAULT NULL,
+  PRIMARY KEY (`account`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `shipping_address` */
+
+insert  into `shipping_address`(`consignee`,`account`,`region`,`detailed_address`,`phone-number`,`tel`,`postcode`,`acquiescent`) values ('收货人1','XDDDB','地区','详细地址','98765432101',NULL,411212,1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
