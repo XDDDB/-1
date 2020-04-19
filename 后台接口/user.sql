b070a41c2f4b0fdf0f1caa906b57f4f4736b8039
XDDDB <51122108+XDDDB@users.noreply.github.com>
2020-04-19 10:14:46 +0800

    Add files via upload

---
+++ 后台接口/user.sql
@@ -0,0 +1,46 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 8.0.16 : Database - xiangmu
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`xiangmu` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `xiangmu`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `account` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `real_name` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `detailed_address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `postcode` int(10) DEFAULT NULL,
+  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Email` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `tel` int(20) DEFAULT NULL,
+  `qq` char(15) DEFAULT NULL,
+  `password` char(20) NOT NULL,
+  `status` int(1) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`account`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `user` */
+
+insert  into `user`(`account`,`real_name`,`detailed_address`,`postcode`,`phone`,`Email`,`tel`,`qq`,`password`,`status`,`id`) values ('ertt','www',NULL,40000,'13984563212','ccc@.qqcom',NULL,'1452365120','789412035',NULL,2),('www',NULL,NULL,NULL,'13456245698','qwe@qq.com',NULL,NULL,'456789123',NULL,8),('XDDDB','张三','详细地址',410000,'15647894561','123@qq.com',NULL,'1234567891','123456789',NULL,6),('zhh',NULL,NULL,NULL,'18975643124','165@zbd.com',NULL,NULL,'456789123',NULL,7);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
