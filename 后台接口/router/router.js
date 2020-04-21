5f64ac54ad6efd3577651fe086c18e95572705db
XDDDB <51122108+XDDDB@users.noreply.github.com>
2020-04-21 15:40:26 +0800

    Add files via upload

---
+++ 后台接口/router/router.js
@@ -0,0 +1,14 @@
+//路由回调函数放入router.js中
+var login =require('./login')
+var goods =require('./goods')
+var demo=require('./demo')
+var journalism=require('./journalism')
+var user=require('./user')
+var shopping_cart=require('./shopping_cart')
+var order_management=require('./order_management')
+//导出一个方法
+// extend合并对象
+// Object.assign
+
+var router = Object.assign({},login,goods,demo,journalism,user,shopping_cart,order_management)
+module.exports=router
